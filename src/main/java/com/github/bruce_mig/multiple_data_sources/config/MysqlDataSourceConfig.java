0fa26f59115801adb0c9f23c8df68a68dd837bd6
bruce-mig <brucetmig@icloud.com>
2025-01-28 21:28:52 +0200

    set mysql as primary

--- src/main/java/com/github/bruce_mig/multiple_data_sources/config/MysqlDataSourceConfig.java
+++ src/main/java/com/github/bruce_mig/multiple_data_sources/config/MysqlDataSourceConfig.java
@@ -1,27 +1,24 @@
 package com.github.bruce_mig.multiple_data_sources.config;
 
 import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.init.DataSourceScriptDatabaseInitializer;
-import org.springframework.boot.sql.init.DatabaseInitializationMode;
-import org.springframework.boot.sql.init.DatabaseInitializationSettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
-import javax.sql.DataSource;
-import java.util.List;
 
 @Configuration(proxyBeanMethods = false)
 public class MysqlDataSourceConfig {
 
-    @Bean
+
     @ConfigurationProperties("app.datasource.blog")
+    @Bean
     public DataSourceProperties blogDataSourceProperties() {
         return new DataSourceProperties();
     }
 
+    @Primary
     @Bean
     public HikariDataSource blogDataSource(DataSourceProperties blogDataSourceProperties) {
         return blogDataSourceProperties.initializeDataSourceBuilder()
